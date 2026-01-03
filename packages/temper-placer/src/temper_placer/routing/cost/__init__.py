e18de354b1292bb1447ff708aa05336d413eeef1
Bennet Leff <bennetleff@gmail.com>
2026-01-03 11:08:45 -0600

    feat(router): Extract NeighborCost module
    - Created routing/cost/neighbor_cost.py with neighbor cost functions
    - Functions: check_blocked, check_net_isolation, compute_sharing_penalty, get_strategy_multiplier, compute_layer_balance_cost, compute_base_cost, compute_congestion_multiplier
    - Handles blocked cell checking, net isolation (DRC-1), occupancy status, congestion-aware sharing penalties
    - 30 unit tests added

--- packages/temper-placer/src/temper_placer/routing/cost/__init__.py
+++ packages/temper-placer/src/temper_placer/routing/cost/__init__.py
@@ -1,4 +1,4 @@
-"""Layer cost calculations for maze routing."""
+"""Cost calculations for maze routing."""
 
 from .layer_cost import (
     compute_congestion_cost,
@@ -9,13 +9,31 @@ from .layer_cost import (
     compute_via_cost,
     compute_wrong_way_penalty,
 )
+from .neighbor_cost import (
+    BLOCKED_COST,
+    check_blocked,
+    check_net_isolation,
+    compute_base_cost,
+    compute_congestion_multiplier,
+    compute_layer_balance_cost,
+    compute_sharing_penalty,
+    get_strategy_multiplier,
+)
 
 __all__ = [
+    "BLOCKED_COST",
+    "check_blocked",
+    "check_net_isolation",
+    "compute_base_cost",
     "compute_congestion_cost",
+    "compute_congestion_multiplier",
+    "compute_layer_balance_cost",
     "compute_layer_balance_penalty",
     "compute_layer_preference_penalty",
+    "compute_sharing_penalty",
     "compute_strategy_multiplier",
     "compute_total_move_cost",
     "compute_via_cost",
     "compute_wrong_way_penalty",
+    "get_strategy_multiplier",
 ]
