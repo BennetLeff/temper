db8b361dce8860a4b2606344bf7ea374bb0d49bf
Bennet Leff <bennetleff@gmail.com>
2026-01-02 19:57:40 -0600

    feat: Add via optimizer and post-processing pipeline integration
    - Created via_optimizer.py with ViaOptimizer class for via consolidation,
      repositioning, and elimination
    - Added GeometricNudger and TraceBallooner exports to post_processing/__init__.py
    - Integrated _run_post_processing() into MazeRouter to run via optimization,
      trace nudging, and trace ballooning after routing completes

--- packages/temper-placer/src/temper_placer/routing/post_processing/__init__.py
+++ packages/temper-placer/src/temper_placer/routing/post_processing/__init__.py
@@ -6,5 +6,26 @@ from temper_placer.routing.post_processing.length_matcher import (
     LengthMatcher,
     SerpentineParams,
 )
+from temper_placer.routing.post_processing.nudger import (
+    GeometricNudger,
+    Node,
+)
+from temper_placer.routing.post_processing.trace_ballooner import (
+    TraceBallooner,
+)
+from temper_placer.routing.post_processing.via_optimizer import (
+    ViaOptimizer,
+    ViaOptimizationStats,
+)
 
-__all__ = ["FunnelSmoother", "Point", "LengthMatcher", "SerpentineParams"]
+__all__ = [
+    "FunnelSmoother",
+    "Point",
+    "LengthMatcher",
+    "SerpentineParams",
+    "GeometricNudger",
+    "Node",
+    "TraceBallooner",
+    "ViaOptimizer",
+    "ViaOptimizationStats",
+]
