1d9151ce87bb5f11733dcf31fa83eb837a5f6b93
BennetLeff <BennetLeff@users.noreply.github.com>
2026-09-02 02:40:07 +0000

    chore(dashboard): deploy 56dd23414445467f96d3fdca4154c575ace525f4 56dd23414445467f96d3fdca4154c575ace525f4

--- architecture/build-info.js
+++ architecture/build-info.js
@@ -1,4 +1,4 @@
 globalThis.TEMPER_ATLAS_BUILD = Object.freeze({
-  "commit": "96baf2dcd8d7d036d38e7cc656709106a6788762",
+  "commit": "56dd23414445467f96d3fdca4154c575ace525f4",
   "ref": "main"
 });
