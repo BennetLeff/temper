3bc0e82854efdd6dafe730dfa0bec0dc07061598
BennetLeff <BennetLeff@users.noreply.github.com>
2026-09-03 18:47:18 +0000

    chore(dashboard): deploy faca9d05270000ee194f48f511736d7f1745c636 faca9d05270000ee194f48f511736d7f1745c636

--- architecture/build-info.js
+++ architecture/build-info.js
@@ -1,4 +1,4 @@
 globalThis.TEMPER_ATLAS_BUILD = Object.freeze({
-  "commit": "87625ab4fa1065bd0531f1a772b7be9eada47009",
+  "commit": "faca9d05270000ee194f48f511736d7f1745c636",
   "ref": "main"
 });
